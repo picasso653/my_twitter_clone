7b10efe230c5fa58d1060beecceb955297deb3ca
Isaac Korankye Asante <xquisit31@gmail.com>
2023-09-24 10:17:53 +0000

    I have a lot to work on

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,57 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:my_twitter_clone/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:my_twitter_clone/state/suggestionUserState.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'package:my_twitter_clone/state/searchState.dart';
+import 'package:my_twitter_clone/ui/page/common/locator.dart';
+import 'package:my_twitter_clone/ui/theme/theme.dart';
+
+import 'helper/routes.dart';
+import 'state/appState.dart';
+import 'state/authState.dart';
+import 'state/chats/chatState.dart';
+import 'state/feedState.dart';
+import 'state/notificationState.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  setupDependencies();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+        ChangeNotifierProvider<AuthState>(create: (_) => AuthState()),
+        ChangeNotifierProvider<FeedState>(create: (_) => FeedState()),
+        ChangeNotifierProvider<ChatState>(create: (_) => ChatState()),
+        ChangeNotifierProvider<SearchState>(create: (_) => SearchState()),
+        ChangeNotifierProvider<NotificationState>(
+            create: (_) => NotificationState()),
+        ChangeNotifierProvider<SuggestionsState>(
+            create: (_) => SuggestionsState()),
+      ],
+      child: MaterialApp(
+        title: 'Fwitter',
+        theme: AppTheme.appTheme.copyWith(
+          textTheme: GoogleFonts.mulishTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
         debugShowCheckedModeBanner: false,
-        title: 'Twitter Clone',
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFE029A)),
-            buttonTheme: const ButtonThemeData(
-           buttonColor: Color.fromARGB(255, 137, 255, 82),
-           shape: RoundedRectangleBorder()),
-            textTheme: const TextTheme(
-              bodyLarge: TextStyle(color: Colors.blueAccent),
-            ),
-            
-            ),
-        home: LoginScreen());
+        routes: Routes.route(),
+        onGenerateRoute: (settings) => Routes.onGenerateRoute(settings),
+        onUnknownRoute: (settings) => Routes.onUnknownRoute(settings),
+        initialRoute: "SplashPage",
+      ),
+    );
   }
 }
---
+++ lib/ui/page/common/locator.dart
@@ -0,0 +1,8 @@
+import 'package:my_twitter_clone/helper/shared_prefrence_helper.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupDependencies() {
+  getIt.registerSingleton<SharedPreferenceHelper>(SharedPreferenceHelper());
+}
