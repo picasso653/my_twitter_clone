5f4c64be2b0773602816a713bd666fd42b476af9
Isaac Korankye Asante <xquisit31@gmail.com>
2023-09-25 17:59:49 +0000

    save this code

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
             create: (_) => SuggestionsState()),
       ],
       child: MaterialApp(
-        title: 'Fwitter',
+        title: 'Y',
         theme: AppTheme.appTheme.copyWith(
           textTheme: GoogleFonts.mulishTextTheme(
             Theme.of(context).textTheme,
